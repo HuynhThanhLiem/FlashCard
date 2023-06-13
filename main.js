1bd7120365292d0083aa89912d669a33e6efb7db
Huynh Thanh Liem <18520982@gm.uit.edu.vn>
2023-06-13 17:15:45 +0700

    swap-card

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
+var selectedCard = 1;
+
 $(document).ready(function() {
-    $('#card-1 .card__inner').on('click', function() {
-
-        $(this).toggleClass('is-flipped');
-
-        $('#card-1 .main__button').toggleClass('is-flipped');
-
-        setTimeout(function() {
-            $('.card__check').fadeIn();
-        }, 3000);
-        
-    });
-
-    $('#play').on('click', function(e) {
-        e.stopPropagation();
-        $('#my-audio').play();
-    });
-
-    $('.main__button .menu-icon').on('click', function() {
-        $('#card-2').addClass('active');
-        $('#card-1').css('z-index','-1');
-    });
+    card.initEvent();
 })
+
+var card = {
+    initEvent: function() {
+        card.flip();
+        card.playAudio();
+        card.skip();
+    },
+
+    flip: function() {
+        $('.card .card__inner').off().on('click', function() {
+            // Flip
+            $(this).toggleClass('is-flipped');
+            
+            // Button appear
+            $('.card.card-selected .main__button').toggleClass('is-flipped');
+            
+            // Check box appear
+            setTimeout(function() {
+                $('.card__check').fadeIn();
+            }, 3000);
+            
+        });
+    },
+
+    playAudio: function() {
+        $('#play').on('click', function(e) {
+            e.stopPropagation();
+            $('#my-audio').play();
+        });
+    },
+
+    skip: function() {
+        $('.main__button .menu-icon').off().on('click', function() {
+            $cardAfter = $('.card.card-after');
+            $cardSelected = $('.card.card-selected');
+
+            $cardAfter.addClass('active');
+            $cardSelected.css('z-index','-1');
+
+            setTimeout(function() {
+                $cardAfter.addClass('card-selected').removeClass('card-after').removeClass('active');
+                $cardSelected.addClass('card-after').removeClass('card-selected');
+            }, 1000);
+
+        });
+    }
+}
