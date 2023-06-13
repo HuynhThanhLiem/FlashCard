9b2b62db3176f4d0f345b341e1b0cd95baedb0a2
Huynh Thanh Liem <18520982@gm.uit.edu.vn>
2023-06-13 14:11:35 +0700

    swap

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
     $('#card-1 .card__inner').on('click', function() {
+
         $(this).toggleClass('is-flipped');
+
         $('#card-1 .main__button').toggleClass('is-flipped');
+
+        setTimeout(function() {
+            $('.card__check').fadeIn();
+        }, 3000);
+        
     });
 
     $('#play').on('click', function(e) {
         e.stopPropagation();
         $('#my-audio').play();
+    });
+
+    $('.main__button .menu-icon').on('click', function() {
+        $('#card-2').addClass('active');
+        $('#card-1').css('z-index','-1');
     });
 })
